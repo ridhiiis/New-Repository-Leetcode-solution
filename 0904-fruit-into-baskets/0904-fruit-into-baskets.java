8139dd75e54a98c26cee8ee7cab63fcc356ef229
Ridhi Sukhani <ridhisukhani@gmail.com>
2026-08-08 22:15:51 +0530

    Time: 3 ms (100%), Space: 71.5 MB (10.26%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.java
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.java
@@ -1,26 +1,50 @@
-import java.util.HashMap;
-
 class Solution {
-    public int totalFruit(int[] fruits) {
-        HashMap<Integer, Integer> basket = new HashMap<>();
-        int left = 0;
-        int maxCount = 0;
-
-        for (int right = 0; right < fruits.length; right++) {
-            basket.put(fruits[right], basket.getOrDefault(fruits[right], 0) + 1);
-
-            while (basket.size() > 2) {
-                int leftFruit = fruits[left];
-                basket.put(leftFruit, basket.get(leftFruit) - 1);
-                if (basket.get(leftFruit) == 0) {
-                    basket.remove(leftFruit);
-                }
-                left++;
-            }
-
-            maxCount = Math.max(maxCount, right - left + 1);
+    static {
+        for(int i = 0; i < 500; ++i) {
+            totalFruit(new int[] {1, 2, 1, 2, 2, 2, 2, 3, 4, 4, 5, 6, 7, 8, 5, 4});
         }
-
-        return maxCount;
+    }
+    public static int totalFruit(int[] fruits) {
+        if(fruits.length == 1) return 1;
+        int i = 0;
+        int j = 0;
+        int f1 = fruits[0];
+        int f1i = 0;
+        int f2 = -1;
+        int f2i = -1;
+        while(j < fruits.length) {
+            if(fruits[j] == f1) {
+                f1i = j;
+            }else if(fruits[j] == f2) {
+                f2i = j;
+            }else if(f2 == -1) {
+                f2 = fruits[j];
+                f2i = j;
+            }else {
+                break;
+            }
+            j += 1;
+        }
+        int maxFruits = j - i;
+        while(j < fruits.length) {
+            if(fruits[j] == f1) {
+                f1i = j;
+            }else if(fruits[j] == f2) {
+                f2i = j;
+            }else {
+                if(f1i < f2i) {
+                    f1 = fruits[j];
+                    i = f1i + 1;
+                    f1i = j;
+                }else {
+                    f2 = fruits[j];
+                    i = f2i + 1;
+                    f2i = j;
+                }
+            }
+            maxFruits = Math.max(maxFruits, j - i + 1);
+            j += 1;
+        }
+        return maxFruits;
     }
 }
